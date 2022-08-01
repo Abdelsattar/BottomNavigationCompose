4e353315d07d6f8538102ee8bf56cf30ef3e45b4
Sattar <sattar@jucr.de>
2022-08-01 20:34:51 +0200

    1- Add our screens

--- app/src/main/java/com/sattar/mybottomnavigation/MainActivity.kt
+++ app/src/main/java/com/sattar/mybottomnavigation/MainActivity.kt
@@ -3,13 +3,7 @@ package com.sattar.mybottomnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.sattar.mybottomnavigation.ui.MainScreen
 import com.sattar.mybottomnavigation.ui.theme.MyBottomNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyBottomNavigationTheme {
-
+                MainScreen()
             }
         }
     }
